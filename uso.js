f899c94b1a1e5eb4e59b7f891caf4fa4f104dd5e
Valorie Portem <valorieportem@gmail.com>
2024-11-04 17:59:09 +0800

    git commit -m "initial commit"

---
+++ uso.js
@@ -0,0 +1,11 @@
+
+let userProfile = {
+    fullName: "Valorie Portem",
+    age: 19,
+    favoriteNumber: 21,
+    favoriteColors: ["White", "Blue", "Peach"]
+  };
+  
+
+  console.log(userProfile);
+  
